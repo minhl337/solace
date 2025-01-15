49391092373fd8646e301c9784e2af3778e9e8dc
minh ha <imnotminh@gmail.com>
2025-01-15 16:55:19 -0600

    env changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
+import * as dotenv from "dotenv"
+
+// Load environment variables from .env file
+dotenv.config()
+
 const config = {
   dialect: "postgresql",
-  schema: "./src/db/schema.ts",
+  schema: "./src/db/schema.ts", // Path to your Drizzle schema file
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
-};
+}
 
-export default config;
+export default config
